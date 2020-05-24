31c7171af513204526115c686fdbf8769424e97d
Justin Pinkney <justinpinkney@gmail.com>
2020-05-24 20:09:28 +0100

    add todo.cfg

--- config.fish
+++ config.fish
@@ -1,5 +1,7 @@
 alias venv="source venv/bin/activate.fish"
-
+alias addvenv="python -m ipykernel install --user --name venv --display-name "
+alias nb="venv/bin/jupyter-notebook"
+                                
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 eval /home/justin/miniconda3/bin/conda "shell.fish" "hook" $argv | source
